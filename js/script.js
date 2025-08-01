f35646b3c1ebea1b87852d1d966cdfc249af0125
Vladislav <64088661+alteropt@users.noreply.github.com>
2025-08-01 14:51:19 +0300

    removed scss, added textures sections

--- js/script.js
+++ js/script.js
@@ -1,158 +1,185 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+document.addEventListener('DOMContentLoaded', function() {
+    const cardSwiper = new Swiper('.card', {
+        spaceBetween: 100,
+        slidesPerView: 1,
+        loop: true,
 
-const cardSwiper = new Swiper('.card', {
-  spaceBetween: 100,
-  slidesPerView: 1,
-  loop: true,
+        navigation: {
+            nextEl: '.card-next',
+            prevEl: '.card-prev',
+        },
 
-  navigation: {
-    nextEl: '.card-next',
-    prevEl: '.card-prev',
-  },
+        pagination: {
+            bulletClass: 'card-bullet',
+            bulletActiveClass: 'card-bullet-active',
+            el: '.card-pagination',
+            clickable: true,
+        }
+    })
 
-  pagination: {
-    bulletClass: 'card-bullet',
-    bulletActiveClass: 'card-bullet-active',
-    el: '.card-pagination',
-    clickable: true,
-  }
-})
+    const reviewsSwiper = new Swiper('.reviews', {
+        spaceBetween: 40,
+        loop: true,
+        slidesPerView: 1.3,
+        autoplay: true,
 
-const reviewsSwiper = new Swiper('.reviews', {
-  spaceBetween: 40,
-  loop: true,
-  slidesPerView: 1.3,
-  autoplay: true,
+        breakpoints: {
+            1920: {
+                slidesPerView: 4.5
+            },
 
-  breakpoints: {
-    1920: {
-      slidesPerView: 4.5
-    },
-    
-    1440: {
-      slidesPerView: 3.5,
-    },
+            1440: {
+                slidesPerView: 3.5,
+            },
 
-    1024: {
-      slidesPerView: 2.2
-    }
-  }
-})
+            1024: {
+                slidesPerView: 2.2
+            }
+        }
+    })
 
-document.querySelectorAll('.faq__question').forEach(el => {
-  el.addEventListener('click', () => {
-    el.classList.toggle('active')
-    el.querySelector('.faq__btn').classList.toggle('active')
-  })
-})
-
-const headerInner = document.querySelector('.header__inner')
-const headerTrigger = document.querySelector('.burger')
-const body = document.querySelector('body')
-
-headerTrigger?.addEventListener('click', () => {
-  body.classList.toggle('popup-mode')
-  headerInner.classList.toggle('active')
-})
-
-const swiperPubl = new Swiper('.publications-swiper', {
-  enabled: true,
-  slidesPerView: 1,
-  loop: true,
-  spaceBetween: 20,
-  autoplay: true,
-  breakpoints: {
-    768: {
-      enabled: false
-    }
-  }
-})
-
-const products = document.querySelectorAll('.product .swiper')
-products.forEach(product => {
-  const swiperProductBackground = new Swiper(product, {
-    loop: true,
-    autoplay: {
-      pauseOnMouseEnter: true,
-      delay: 3000
-    },
-    slidesPerView: 1,
-    pagination: {
-      el: '.product-background-pagination',
-      type: 'bullets',
-      clickable: true,
-    },
-  })
-})
-
-const asideLis = document.querySelectorAll('.aside__nav li')
-const catalogs = document.querySelectorAll('.catalog')
-asideLis.forEach(li => {
-  li.addEventListener('click', function () {
-    const dataName = li.dataset.products
-    catalogs.forEach(catalog => {
-      if(catalog.dataset.products === dataName) {
-        catalog.classList.add('active')
+    document.querySelectorAll('.faq__question').forEach(el => {
+      if(!el.classList.contains('faq__clickable-content')) {        
+        el.addEventListener('click', () => {            
+            el.classList.toggle('active')
+            el.querySelector('.faq__btn').classList.toggle('active')
+          })
       } else {
-        catalog.classList.remove('active')
-      }
+        el.querySelector('.faq__question-head').addEventListener('click', () => {
+            el.classList.toggle('active')
+            el.querySelector('.faq__btn').classList.toggle('active')
+        })
+      }    
     })
-    
-    asideLis.forEach(asideLi => {
-      asideLi.classList.remove('active')
+
+    const headerInner = document.querySelector('.header__inner')
+    const headerTrigger = document.querySelector('.burger')
+    const body = document.querySelector('body')
+
+    headerTrigger?.addEventListener('click', () => {
+        body.classList.toggle('popup-mode')
+        headerInner.classList.toggle('active')
     })
-    li.classList.add('active')
 
-  })
-})
+    const swiperPubl = new Swiper('.publications-swiper', {
+        enabled: true,
+        slidesPerView: 1,
+        loop: true,
+        spaceBetween: 20,
+        autoplay: true,
+        breakpoints: {
+            768: {
+                enabled: false
+            }
+        }
+    })
 
-const popupTriggers = document.querySelectorAll('#order')
-const popup = document.querySelector('.popup')
-const popupContent = document.querySelector('.popup__content')
-const popupClose = document.querySelector('.popup__close')
+    const products = document.querySelectorAll('.product .swiper')
+    products.forEach(product => {
+        const swiperProductBackground = new Swiper(product, {
+            loop: true,
+            autoplay: {
+                pauseOnMouseEnter: true,
+                delay: 3000
+            },
+            slidesPerView: 1,
+            pagination: {
+                el: '.product-background-pagination',
+                type: 'bullets',
+                clickable: true,
+            },
+        })
+    })
 
-document.addEventListener( 'click', (e) => {
-	const withinBoundaries = e.composedPath().includes(popupContent)
+    const asideLis = document.querySelectorAll('.aside__nav li')
+    const catalogs = document.querySelectorAll('.catalog')
+    asideLis.forEach(li => {
+        li.addEventListener('click', function() {
+            const dataName = li.dataset.products
+            catalogs.forEach(catalog => {
+                if(catalog.dataset.products === dataName) {
+                    catalog.classList.add('active')
+                } else {
+                    catalog.classList.remove('active')
+                }
+            })
 
-	if ( !withinBoundaries && e.target.getAttribute('id') !== 'order' && popup.classList.contains('active')) {
-		popup.classList.remove('active')
-    body.classList.remove('popup-mode')
-	}
-})
+            asideLis.forEach(asideLi => {
+                asideLi.classList.remove('active')
+            })
+            li.classList.add('active')
 
-popupClose.addEventListener('click', function() {
-  popup.classList.remove('active')
-  body.classList.remove('popup-mode')
-})
+        })
+    })
 
-const popupForm = document.querySelector('.popup form')
-popupForm.addEventListener('submit', function() {
-  popup.classList.remove('active')
-  body.classList.remove('popup-mode')
-})
+    const popupTriggers = document.querySelectorAll('.order-button')
+    const popup = document.querySelector('.popup')
+    const popupContent = document.querySelector('.popup__content')
+    const popupClose = document.querySelector('.popup__close')
 
-popupTriggers.forEach(popupTrigger => {
-  popupTrigger.addEventListener('click', function() {
-    popup.classList.add('active')
-    body.classList.add('popup-mode')
-  })
-})
+    document.addEventListener('click', (e) => {
+        const withinBoundaries = e.composedPath().includes(popupContent)
+        
+
+        // if(!withinBoundaries && e.target.getAttribute('id') !== 'order' && popup.classList.contains('active')) {
+        if(!withinBoundaries && !e.target.classList.contains('order-button') && popup.classList.contains('active')) {
+            popup.classList.remove('active')
+            body.classList.remove('popup-mode')
+        }
+    })
+
+    popupClose.addEventListener('click', function() {
+        popup.classList.remove('active')
+        body.classList.remove('popup-mode')
+    })
+
+    const popupForm = document.querySelector('.popup form')
+    popupForm.addEventListener('submit', function() {
+        popup.classList.remove('active')
+        body.classList.remove('popup-mode')
+    })
+
+    popupTriggers.forEach(popupTrigger => {
+        popupTrigger.addEventListener('click', function() {
+            popup.classList.add('active')
+            body.classList.add('popup-mode')
+        })
+    })
 
 
-const productSwiper = new Swiper('.more__picture', {
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 12,
-  pagination: {
-    el: '.more__tabs',
-    bulletClass: 'more__tab',
-    bulletActiveClass: 'more__tab-active',
-    type: 'bullets',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.more-next',
-    prevEl: '.more-prev'
-  }
-})
+    const productSwiper = new Swiper('.more__picture', {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 12,
+        pagination: {
+            el: '.more__tabs',
+            bulletClass: 'more__tab',
+            bulletActiveClass: 'more__tab-active',
+            type: 'bullets',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.more-next',
+            prevEl: '.more-prev'
+        }
+    });
+
+    // const texturesZoomTriggers = document.querySelectorAll('.zoom-texture-btn')
+    // texturesZoomTriggers.forEach(texturesZoomTrigger => {
+    //   texturesZoomTrigger.addEventListener('click', () => {
+    //     texturesZoomTrigger.parentNode.parentNode.parentNode.classList.toggle('active')
+    //     body.classList.toggle('popup-mode')
+    //   })
+    // })
+    lightGallery(document.querySelector('.faq__textures', {
+      selector: ".faq__texture-img"
+    }))
+
+    document.querySelector('.faq__texture-download').addEventListener('click', (e) => {
+      e.stopPropagation()
+    })
+    document.querySelector('.faq__texture-more h4').addEventListener('click', (e) => {
+      e.stopPropagation()
+    })
+});
 
